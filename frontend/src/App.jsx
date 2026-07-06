9c8aac54e8cbb39f1c1a9e56c2c165168dd9db82
ManojAhire <manoj.332211.1@gmail.com>
2026-07-06 11:25:41 +0530

    project setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import Sidebar from "./components/Sidebar";
+import Navbar from "./components/Navbar";
+import Dashboard from "./pages/Dashboard";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Sidebar />
+      <Dashboard />
+    </>
+  );
+}
+
+export default App;
