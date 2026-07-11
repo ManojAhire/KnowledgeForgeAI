7c7ce315bc647b75981b7521ce0951ddaf2a0c59
ManojAhire <manoj.332211.1@gmail.com>
2026-07-11 10:47:24 +0530

    Implemented PDF upload with React and FastAPI

---
+++ backend/app/main.py
@@ -0,0 +1,26 @@
+from fastapi import FastAPI
+from app.routes.upload import router as upload_router
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI(
+    title="KnowledgeForge AI"
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:5173",
+        "http://localhost:5174",
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.include_router(upload_router)
+
+@app.get("/")
+def home():
+    return {
+        "message": "KnowledgeForge AI Backend Running 🚀"
+    }
--- frontend/src/pages/Upload.jsx
+++ frontend/src/pages/Upload.jsx
@@ -2,12 +2,47 @@ import {useState} from "react";
 import UploadBox from "../components/UploadBox";
 
 
-
 function Upload() {
 
-    const [SelectedFile, setSelectedFile] = useState(null);
+    const [selectedFile, setSelectedFile] = useState(null);
 
+    const handleUpload = async () => {
 
+  if (!selectedFile) {
+    alert("Please select a PDF.");
+    return;
+  }
+
+  const formData = new FormData();
+
+  formData.append("file", selectedFile);
+
+  try {
+
+    const response = await fetch(
+      "http://127.0.0.1:8000/upload",
+      {
+        method: "POST",
+        body: formData,
+      }
+    );
+
+    const data = await response.json();
+
+    console.log(data);
+
+    alert(data.message);
+
+  } catch (error) {
+
+    console.error(error);
+
+    alert("Upload failed.");
+
+  }
+
+};
+    
 
   return (
     <div className="dashboard" >
@@ -21,12 +56,12 @@ function Upload() {
             <h3>Selected File</h3>
 
             <p>
-                {SelectedFile ? SelectedFile.name : "No file selected"}
+                {selectedFile ? selectedFile.name : "No file selected"}
             </p>
 
         </div>
 
-        <button className="upload-b">
+        <button className="upload-b" onClick={handleUpload}>
             Upload
         </button>
         
